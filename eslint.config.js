e6c8e62f09190ca1a572e10bd5c1dd4d8f1b1025
joyscripts <joyscripts25@gmail.com>
2025-12-13 22:01:16 +0900

    chore: React Hooks의 추천 규칙을 적용 셋팅 추가

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+  reactHooks.configs.flat.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
